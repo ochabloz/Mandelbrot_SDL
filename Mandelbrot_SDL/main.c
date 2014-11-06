f5a5e61d0094e598c8d6150659867e023ad695d3
ochabloz <ochabloz@gmail.com>
2014-11-06 14:48:20 +0100

    Initial

---
+++ Mandelbrot_SDL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Mandelbrot_SDL
+//
+//  Created by Olivier on 06.11.14.
+//  Copyright (c) 2014 Olivier. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   printf("Hello, World!\n");
+    return 0;
+}

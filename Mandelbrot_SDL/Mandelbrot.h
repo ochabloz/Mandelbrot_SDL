73b3e65b0d7dca84cde90d65c6b35f032f8c2485
Alexis Marquet <Alexismarquet@Alexiss-MacBook-Pro.local>
2014-11-06 15:41:02 +0100

    mandelbort & pile.h

---
+++ Mandelbrot_SDL/Mandelbrot.h
@@ -0,0 +1,13 @@
+//
+//  Mandelbrot.h
+//  Mandelbrot_SDL
+//
+//  Created by Alexis Marquet on 06/11/14.
+//  Copyright (c) 2014 Olivier. All rights reserved.
+//
+
+#ifndef Mandelbrot_SDL_Mandelbrot_h
+#define Mandelbrot_SDL_Mandelbrot_h
+
+
+#endif
---
+++ Mandelbrot_SDL/Pile.h
@@ -0,0 +1,15 @@
+//
+//  Pile.h
+//  Mandelbrot_SDL
+//
+//  Created by Alexis Marquet on 06/11/14.
+//  Copyright (c) 2014 Olivier. All rights reserved.
+//
+
+#ifndef Mandelbrot_SDL_Pile_h
+#define Mandelbrot_SDL_Pile_h
+
+int nb_elements(
+bloc_M_t* pop (*pile);
+void push (*pile), *bloc_M_t);
+#endif
